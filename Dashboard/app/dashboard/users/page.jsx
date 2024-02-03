b0b0a434720cf60833329f95bd2113fb6bc3fd59
krish <sharmageeta847@gmail.com>
2024-02-03 16:09:07 +0530

    acheivement completer

--- Dashboard/app/dashboard/users/page.jsx
+++ Dashboard/app/dashboard/users/page.jsx
@@ -1,25 +1,22 @@
-"use client"
-// import { deleteUser } from "@/app/lib/actions";
-// import { fetchUsers } from "@/app/lib/data";
-import Pagination from "@/app/ui/dashboard/pagination/pagination";
+'use client'
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const UsersPage = async ({ searchParams }) => {
-  const q = searchParams?.q || "";
-  const page = searchParams?.page || 1;
-  const [users, setUsers] = useState([])
-  // const { count, users } = await fetchUsers(q, page);
+const UsersPage =  () => {
+
+
+  const [data, setData] = useState([]);
+
    useEffect(()=>{
-   
-      const fetchAchievements = async ()=>{
+       const fetchAchievements = async ()=>{
         try {
-        const res = await fetch('http://localhost:5000/api/achievements');
-        const data = await res.json();
-        setUsers(data);
+        const response = await fetch('http://localhost:5000/api/achievement');
+        const data = await response.json();
+        setData(data);
+        console.log(data);
       } catch (error) {
       console.log(error)
     }
@@ -42,18 +39,17 @@ const UsersPage = async ({ searchParams }) => {
             <td>Title</td>
             <td>Description</td>
             <td>Image</td>
-            <td>Image</td>
             <td>Created At</td>
             <td>Action</td>
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              {/* <td>
+          {data.map((user, index) => (
+            <tr key={user._id}>
+              <td>
                 <div className={styles.user}>
                   <Image
-                    src={user.img || "/noavatar.png"}
+                    src={user.imageUrl || "/noavatar.png"}
                     alt=""
                     width={40}
                     height={40}
@@ -61,37 +57,19 @@ const UsersPage = async ({ searchParams }) => {
                   />
                   {user.username}
                 </div>
-              </td> */}
+              </td>
               <td>{user.title}</td>
               <td>{user.description}</td>
-              <td><div className={styles.user}>
-                  <Image
-                    src={user.imageUrl1 || "/noavatar.png"}
-                    alt=""
-                    width={40}
-                    height={40}
-                    className={styles.userImage}
-                  />
-                </div></td>
-                <td><div className={styles.user}>
-                  <Image
-                    src={user.imageUrl1 || "/noavatar.png"}
-                    alt=""
-                    width={40}
-                    height={40}
-                    className={styles.userImage}
-                  />
-                </div></td>
               <td>{user.createdAt?.toString().slice(4, 16)}</td>
               <td>
                 <div className={styles.buttons}>
-                  <Link href={`/dashboard/users/${user.id}`}>
+                  <Link href={''}>
                     <button className={`${styles.button} ${styles.view}`}>
                       View
                     </button>
                   </Link>
-                  <form action="">
-                    <input type="hidden" name="id" value={(user.id)} />
+                  <form >
+                    <input type="hidden" name="id"  />
                     <button className={`${styles.button} ${styles.delete}`}>
                       Delete
                     </button>
--- akgu-backend/controllers/admin.js
+++ akgu-backend/controllers/admin.js
@@ -15,7 +15,7 @@ exports.getFaculty = async (req, res, next) => {
 exports.postFaculty = async (req, res, next) => {
     const data = {
         ...req.body,
-        imageUrl: req.body.imageUrl1.length === 0 ? "" : req.body.imageUrl1
+        imageUrl: req.body.imageUrl.length === 0 ? "" : req.body.imageUrl
     }
     try {
         const existing = await faculty.findOne({ imageUrl: req.body.imageUrl })
@@ -147,8 +147,7 @@ exports.postAchievement = async (req, res, next) => {
         ...req.body,
         title: req.body.title,
         description: req.body.description,
-        imageUrl1: req.body.imageUrl1.length === 0 ? "" : req.body.imageUrl1,
-        imageUrl2: req.body.imageUrl2.length === 0 ? "" : req.body.imageUrl2
+        imageUrl: req.body.imageUrl.length === 0 ? "" : req.body.imageUrl
     }
     try {
         const existing = await achievement.findOne({ title: req.body.title })
